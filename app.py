b349865a41133881d7b7ca343113fc02a30d8766
Ricardo Alanis <ricardo.alanis@gmail.com>
2015-12-11 09:31:28 -0600

    added literal encoding2

--- app.py
+++ app.py
@@ -78,7 +78,8 @@ def gen_feed_info():
     return feed_dict["feed_id"]
 
 r = requests.get("http://148.243.232.113/calidadaire/xml/simat.json")
-stations = r.json()["pollutionMeasurements"]["stations"]
+rtextencoded = r.text().encode("latin-1")
+stations = rtextencoded["pollutionMeasurements"]["stations"]
 units = {"NO2":"ppb","O3":"ppb","SO2":"ppb","PM10":"ug/m3","CO":"ppm"}
 methods = {"O3":"MXMEX-O3-1993","NO2":"MXMEX-NOx-1993","SO2":"MXMEX-SO2-1993","PM10":"MXMEX-PM10-1993", "Temp": "MXMEX-TEMP", "Hum": "MXMEX-HUM", "CO":"MXMEX-CO-1993"}
 
