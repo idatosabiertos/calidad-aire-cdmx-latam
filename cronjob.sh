a43497fd201d63bc4de2d0e96862b37609979ac6
Ricardo Alanis <ricardo.alanis@gmail.com>
2015-12-11 09:56:27 -0600

    added file

--- cronjob.sh
+++ cronjob.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python3 app.py
 zip output.zip output/*.*
-curl -i -X POST -f filedata=output/data.zip "http://104.197.214.72:8000/"
+curl -i -X POST -f filedata=@data.zip "http://104.197.214.72:8000/"
